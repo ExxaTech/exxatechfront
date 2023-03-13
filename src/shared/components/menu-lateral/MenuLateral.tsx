503680cfe1a500a73fad08fe7ac8f064aa17c56f
Milton Reis <Milton.Oliveira-ext@ab-Inbev.com>
2023-03-13 14:11:57 -0300

    feat: add menu Item

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,7 +1,37 @@
-import HomeIcon from '@mui/icons-material/Home';
-import { Avatar, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 import { ReactNode } from 'react';
+import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
+import { useDrawerContext } from '../../contexts';
+
+
+interface IListItemLinkProps {
+  label: string;
+  icon: string;
+  to: string;
+  onClick: (() => void) | undefined;
+}
+
+const ListItemLink: React.FC<IListItemLinkProps> = ({ label, icon, to, onClick }) => {
+
+  const navigate = useNavigate();
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({ path: resolvedPath.pathname, end: false });
+
+  const handleClick = () => {
+    navigate(to);
+    onClick?.();
+  };
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <Icon> {icon} </Icon>
+      </ListItemIcon>
+      <ListItemText primary={label} />
+    </ListItemButton>
+  );
+};
 
 interface IMenuLateralrData {
   children: ReactNode
@@ -9,30 +39,36 @@ interface IMenuLateralrData {
 
 export const MenuLateral: React.FC<IMenuLateralrData> = ({ children }) => {
   const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
 
   return (
     <>
-      <Drawer variant='permanent'>
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
         <Box width={theme.spacing(28)} display="flex" flexDirection="column" height="100%">
           <Box width="25%" height={theme.spacing(20)} display="flex" alignItems="stretch" justifyContent="stretch">
             <Avatar alt="Remy Sharp"
               sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
-              src={window.location.origin + '/logo-docs-256.png'} />
+              src={window.location.origin + '/exxa.png'} />
           </Box>
           <Divider />
           <Box flex={1}>
             <List component="nav">
-              <ListItemButton>
-                <ListItemIcon>
-                  <HomeIcon />
-                </ListItemIcon>
-                <ListItemText primary="Pagina Inicial" />
-              </ListItemButton>
+              <ListItemLink icon='home'
+                to='/pagina-inicial'
+                label='Pagina inicial'
+                onClick={smDown ? toggleDrawerOpen : undefined}
+              />
+              <ListItemLink icon='chat'
+                to='/wpp'
+                label='WhatsApp'
+                onClick={smDown ? toggleDrawerOpen : undefined}
+              />
             </List>
           </Box>
         </Box>
       </Drawer>
-      <Box height="100vh" marginLeft={theme.spacing(28)}>
+      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
       </Box>
     </>
---
+++ src/shared/contexts/DrawerContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, ReactNode, useCallback, useContext, useState } from 'react';
+
+interface IDrawerContextData {
+  isDrawerOpen: boolean;
+  toggleDrawerOpen: () => void;
+}
+
+const DrawerContext = createContext({} as IDrawerContextData);
+
+export const useDrawerContext = () => {
+  return useContext(DrawerContext);
+};
+
+interface IDrawerProviderData {
+  children: ReactNode
+}
+
+export const DrawerProvider: React.FC<IDrawerProviderData> = ({ children }) => {
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawerOpen = useCallback(() => {
+    setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen);
+  }, []);
+
+
+  return (
+    <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen }}>
+      {children}
+    </DrawerContext.Provider>
+  );
+};
