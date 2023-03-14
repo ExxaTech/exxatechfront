2452de4d6d3af02fd723af19ee89b21239fe450e
Milton Reis <Milton.Oliveira-ext@ab-Inbev.com>
2023-03-13 22:58:40 -0300

    feat: add toolbar

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,16 @@
+import { BarraDeFerramentas } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
 export const Dashboard = () => {
   return (
-    <LayoutBaseDePagina titulo='Paginal Incial'>
+    <LayoutBaseDePagina
+      titulo='Paginal Incial'
+      barraDeFerramentas={(
+        <BarraDeFerramentas
+          mostrarInputBusca
+          textoBotaoNovo='Nova' />
+      )}
+    >
       Testando
     </LayoutBaseDePagina>
   );
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -2,7 +2,7 @@ import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, List
 import { Box } from '@mui/system';
 import { ReactNode } from 'react';
 import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
-import { useDrawerContext } from '../../contexts';
+import { useAppThemeContext, useDrawerContext } from '../../contexts';
 
 
 interface IListItemLinkProps {
@@ -41,6 +41,7 @@ export const MenuLateral: React.FC<IMenuLateralrData> = ({ children }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
   const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
+  const { toggleTheme } = useAppThemeContext();
 
   return (
     <>
@@ -63,6 +64,16 @@ export const MenuLateral: React.FC<IMenuLateralrData> = ({ children }) => {
                   onClick={smDown ? toggleDrawerOpen : undefined}
                 />
               ))}
+            </List>
+          </Box>
+          <Box>
+            <List component="nav">
+              <ListItemButton onClick={toggleTheme}>
+                <ListItemIcon>
+                  <Icon> dark_mode </Icon>
+                </ListItemIcon>
+                <ListItemText />
+              </ListItemButton>
             </List>
           </Box>
         </Box>
